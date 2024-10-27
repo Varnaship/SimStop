fba5c8e1df99e2f9f3a8839071e5dbfcc2ef8cd8
Kristian Petrov <kriskoplay05@gmail.com>
2024-10-27 18:22:11 +0200

    Adding constants for the database

---
+++ SimStop.Common/Constants/DatabaseConstants.cs
@@ -0,0 +1,30 @@
+﻿namespace SimStop.Common.Constants
+{
+    public static class DatabaseConstants
+    {
+        public const int BrandNameMaxLenght = 10;
+        public const int BrandNameMinLenght = 10;
+
+        public const int BrandDescriptionMaxLenght = 20;
+        public const int BrandDescriptionMinLenght = 10;
+
+        public const int CategoryNameMaxLenght = 20;
+        public const int CategoryNameMinLenght = 10;
+
+        public const int LocationNameMaxLenght = 20;
+        public const int LocationNameMinLenght = 10;
+
+        public const int ProductNameMaxLenght = 20;
+        public const int ProductNameMinLenght = 10;
+
+        public const int ProductDescriptioneMaxLenght = 20;
+        public const int ProductDescriptioneMinLenght = 10;
+
+        public const int ShopNameMaxLenght = 20;
+        public const int ShopNameMinLenght = 10;
+
+        public const string BrandFoundedOnFormat = "MMMM yyyy";
+
+        public const string ProductReleaseDateFormat = "d MMM yyyy";
+    }
+}
