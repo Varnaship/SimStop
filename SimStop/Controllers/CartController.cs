db8b7f9bea91d3d04ba5216d9dfcc356d4910948
Kristian Petrov <kriskoplay05@gmail.com>
2024-11-18 11:50:08 +0200

    Added Cart Controller with Removing methods //TODO:Implement all taks

--- SimStop/Controllers/CartController.cs
+++ SimStop/Controllers/CartController.cs
@@ -8,5 +8,17 @@ namespace SimStop.Web.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> RemoveFromCart(int id)
+        {
+            var model = 1;
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> DeleteFromCart(int id)
+        {
+            var model = 1;
+            return View(model);
+        }
     }
 }
